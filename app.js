eee2a216787da39783d139f1f84b85afdf5c826e
radu <radusamson@ymail.com>
2013-08-13 23:43:25 +0300

    added front files

---
+++ app.js
@@ -0,0 +1,21 @@
+/* starts the player on a canvas*/
+function embed(width, height){
+        var $canvas = $(document.createElement('canvas')).width(width).height(height)
+        $("#player").html($canvas)
+        var canvas = $canvas[0];
+        var context = canvas.getContext('2d')
+        var imageObj = new Image()
+        imageObj.onload= function(){
+                context.drawImage(imageObj, 0, 0, width, height);
+        }
+        //we go with 10 frames/s
+        var interval = setInterval(function(){
+            //we do an http request for latest image provided by ffmpeg
+            imageObj.src="/frame/";
+        , 100})
+}
+
+/* run */
+$(function(){
+        embed(150, 150);
+});
